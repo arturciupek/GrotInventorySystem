02d8f7818447813d93edb87c697f4516cfcff9be
arturciupek <arturtciupek18@gmail.com>
2026-02-23 20:40:28 +0100

    Add module location relation and migration.

---
+++ GrotInventorySystem/Data/Migrations/20260223193719_AddModuleLocation.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace GrotInventorySystem.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddModuleLocation : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<Guid>(
+                name: "LocationId",
+                table: "Modules",
+                type: "uuid",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Modules_LocationId",
+                table: "Modules",
+                column: "LocationId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Modules_Locations_LocationId",
+                table: "Modules",
+                column: "LocationId",
+                principalTable: "Locations",
+                principalColumn: "Id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Modules_Locations_LocationId",
+                table: "Modules");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Modules_LocationId",
+                table: "Modules");
+
+            migrationBuilder.DropColumn(
+                name: "LocationId",
+                table: "Modules");
+        }
+    }
+}
--- GrotInventorySystem/Domain/Entities/Module.cs
+++ GrotInventorySystem/Domain/Entities/Module.cs
@@ -5,4 +5,7 @@ public class Module
     public Guid Id { get; set; }
     public string SerialNumber { get; set; } = default!;
     public string Name { get; set; } = default!;
+
+    public Guid? LocationId { get; set; }
+    public Location? Location { get; set; }
 }
