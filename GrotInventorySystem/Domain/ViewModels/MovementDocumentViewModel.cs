d9f1d3d955d79ca55b084ac0a62c3273c4d9cde8
arturciupek <arturtciupek18@gmail.com>
2026-04-16 12:30:09 +0200

    Add movement documents page.

---
+++ GrotInventorySystem/Domain/ViewModels/MovementDocumentViewModel.cs
@@ -0,0 +1,12 @@
+﻿namespace GrotInventorySystem.Domain.ViewModels;
+
+public class MovementDocumentViewModel
+{
+    public string DocumentNumber { get; set; } = default!;
+    public DateTime CreatedAt { get; set; }
+    public string? WeaponSerialNumber { get; set; }  // numer seryjny broni
+    public string? ModuleName { get; set; }           // nazwa modułu
+    public string? FromLocationName { get; set; }     // nazwa lokalizacji skąd
+    public string? ToLocationName { get; set; }       // nazwa lokalizacji dokąd
+    public string? CreatedByEmail { get; set; }
+}
