85a6a96b29125bc91d8ce63bc607853e7901a4d3
arturciupek <arturtciupek18@gmail.com>
2026-04-24 12:04:47 +0200

    Add reports page.

---
+++ GrotInventorySystem/Domain/ViewModels/ReportViewModel.cs
@@ -0,0 +1,8 @@
+﻿namespace GrotInventorySystem.Domain.ViewModels;
+
+public class ReportViewModel
+{
+    public string LocationName { get; set; } = default!;
+    public int WeaponCount {  get; set; }
+    public int ModuleCount { get; set; }
+}
